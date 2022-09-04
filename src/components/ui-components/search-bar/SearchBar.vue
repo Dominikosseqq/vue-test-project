4f8f33a4cb93d9a5791c2ab157669180a8f3b4e9
Dominik Kisek <domminots@gmail.com>
2022-09-04 18:09:26 +0200

    add apply params

--- src/components/ui-components/search-bar/SearchBar.vue
+++ src/components/ui-components/search-bar/SearchBar.vue
@@ -7,23 +7,28 @@ import { defineComponent } from "vue";
 import { debounce } from "lodash-es";
 import store from "@/store";
 import { GiphFilterInput } from "@/interfaces/giphyApiTypes";
+import { getParamsFromUrl } from "@/helpers/params/getParamsFromUrl";
+import { applyParamsToUrl } from "@/helpers/params/applyParamsToUrl";
 
 export default defineComponent({
   name: "SearchBar",
   data: () => ({
     searchInputText: store.state.searchText as string,
   }),
-  beforeMount() {
-    store.dispatch("getSearchedGiphs", {
-      q: store.state.searchText,
-    } as GiphFilterInput);
+  beforeCreate() {
+    const params = getParamsFromUrl(history);
+    if (Object.keys(params).includes("q")) {
+      store.dispatch("setSearchText", params.q);
+    }
   },
   watch: {
     searchInputText: debounce((newVal: string): void => {
       if (newVal.length) {
         store.dispatch("getSearchedGiphs", { q: newVal } as GiphFilterInput);
+        applyParamsToUrl(history, newVal);
       } else {
         store.dispatch("getTrendingGiphs");
+        applyParamsToUrl(history, newVal);
       }
     }, 1000),
   },
