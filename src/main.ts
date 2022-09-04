5d1d566186b6a14db291fea0a23c316690f6535e
Dominik Kisek <domminots@gmail.com>
2022-09-04 02:45:56 +0200

    add vue-lazyload package and use it as plugin + add img loader to public

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import router from "@/router";
 import store from "@/store";
 import RouterComponent from "@/components/router-component/RouterComponent.vue";
+import VueLazyLoad from "vue-lazyload";
 
-createApp(RouterComponent).use(store).use(router).mount("#app");
+createApp(RouterComponent)
+  .use(store)
+  .use(router)
+  .use(VueLazyLoad)
+  .mount("#app");
