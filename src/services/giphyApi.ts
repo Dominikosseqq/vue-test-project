774a247cb7944bb39ebb8fd73838dcac88db01fc
Dominik Kisek <domminots@gmail.com>
2022-09-05 23:09:12 +0200

    add comments axios function

--- src/services/giphyApi.ts
+++ src/services/giphyApi.ts
@@ -3,12 +3,19 @@ import { cacheAdapterEnhancer } from "axios-extensions";
 const API_URL = process.env.VUE_APP_GIPHY_URL;
 const API_KEY = process.env.VUE_APP_API_KEY;
 
+// prepare axios's request with adapter for cache requests
 const http = axios.create({
   baseURL: API_URL,
   headers: { "Cache-Control": "no-cache" },
   adapter: cacheAdapterEnhancer(axios.defaults.adapter as AxiosAdapter),
 });
 
+/**
+ * apply params with api_key for gettin api data
+ * @param endpoint {string}
+ * @param options {AxiosRequestConfig}
+ * @return {Promise<AxiosResponse>}
+ */
 export const getGiphyRequest = async (
   endpoint: string,
   options: AxiosRequestConfig
