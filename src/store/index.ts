87a58d7a8c5f2db0fd7a318b4f647a328ce831fc
Dominik Kisek <domminots@gmail.com>
2022-09-05 23:09:40 +0200

    add loading, current page and current limit state with mutations and actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { endpointsApiGiphy } from "@/helpers/constants";
+import { ENDPOINTS_API_GIPHY } from "@/helpers/constants";
 import {
   mapFilterInputIntoRequestParams,
   prepareGiphsDataFromRequest,
@@ -11,12 +11,27 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     searchText: "witcher",
+    currentPage: 1,
+    currentLimit: 50,
+    isLoading: false,
     giphsToDisplay: {} as GiphsToDisplay,
   },
   getters: {},
   mutations: {
-    setSearchText(state, payload) {
-      state.searchText = payload;
+    setIsLoading(state, isLoading: boolean) {
+      state.isLoading = isLoading;
+    },
+
+    setSearchText(state, text: string) {
+      state.searchText = text;
+    },
+
+    setCurrentLimit(state, limit: number) {
+      state.currentLimit = limit;
+    },
+
+    setCurrentPage(state, page: number) {
+      state.currentPage = page;
     },
 
     setGiphsToDisplay(state, data: GiphsToDisplay) {
@@ -24,28 +39,52 @@ export default createStore({
     },
   },
   actions: {
-    setSearchText({ commit }, payload) {
-      commit("setSearchText", payload);
+    setSearchText({ commit }, text: string) {
+      commit("setSearchText", text);
+    },
+
+    setCurrentLimit({ commit }, limit: number) {
+      commit("setCurrentLimit", limit);
+    },
+
+    setCurrentPage({ commit }, page: number) {
+      commit("setCurrentPage", page);
+    },
+
+    setIsLoading({ commit }, isLoading: boolean) {
+      commit("setIsLoading", isLoading);
     },
 
     getTrendingGiphs({ commit }, filter: GiphFilterInput) {
-      const endpoint = endpointsApiGiphy.trending;
+      commit("setIsLoading", true);
+
+      const endpoint = ENDPOINTS_API_GIPHY.trending;
       const options = mapFilterInputIntoRequestParams(filter);
 
-      getGiphyRequest(endpoint, options).then((res: AxiosResponse): void => {
-        const giphsToDisplay = prepareGiphsDataFromRequest(res);
-        commit("setGiphsToDisplay", giphsToDisplay);
-      });
+      getGiphyRequest(endpoint, options)
+        .then((res: AxiosResponse): void => {
+          const giphsToDisplay = prepareGiphsDataFromRequest(res);
+          commit("setGiphsToDisplay", giphsToDisplay);
+        })
+        .finally((): void => {
+          commit("setIsLoading", false);
+        });
     },
 
     getSearchedGiphs({ commit }, filter: GiphFilterInput) {
-      const endpoint = endpointsApiGiphy.search;
+      commit("setIsLoading", true);
+
+      const endpoint = ENDPOINTS_API_GIPHY.search;
       const options = mapFilterInputIntoRequestParams(filter);
 
-      getGiphyRequest(endpoint, options).then((res: AxiosResponse): void => {
-        const giphsToDisplay = prepareGiphsDataFromRequest(res);
-        commit("setGiphsToDisplay", giphsToDisplay);
-      });
+      getGiphyRequest(endpoint, options)
+        .then((res: AxiosResponse): void => {
+          const giphsToDisplay = prepareGiphsDataFromRequest(res);
+          commit("setGiphsToDisplay", giphsToDisplay);
+        })
+        .finally((): void => {
+          commit("setIsLoading", false);
+        });
     },
   },
   modules: {},
