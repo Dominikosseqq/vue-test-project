9524048d71c2bf61d62c7e5595032ed8cbaae331
Dominik Kisek <domminots@gmail.com>
2022-09-05 23:08:59 +0200

    add comment to get search query params fun

--- src/helpers/params/getParamsFromUrl.ts
+++ src/helpers/params/getParamsFromUrl.ts
@@ -1,11 +1,16 @@
 import { GiphFilterInput } from "@/interfaces/giphyApiTypes";
-import { keyParamsApiGiphy } from "@/helpers/constants";
+import { KEY_PARAMS_API_GIPHY } from "@/helpers/constants";
+import { LocationQuery } from "vue-router";
 
-export function getParamsFromUrl(history: History): GiphFilterInput {
-  const uri = new URLSearchParams(history.state.current.replace("/?", ""));
-  const filter = [...uri.keys()].reduce((out, key) => {
-    const newObj = keyParamsApiGiphy.includes(key)
-      ? { [key]: uri.get(key) }
+/**
+ * function that get variables (query params) from location
+ * @param query {LocationQuery}
+ * @return {GiphFilterInput}
+ */
+export function getParamsFromUrl(query: LocationQuery): GiphFilterInput {
+  const filter = Object.keys(query).reduce((out, key) => {
+    const newObj = KEY_PARAMS_API_GIPHY.includes(key)
+      ? { [key]: query[key] }
       : {};
     return Object.assign(out, newObj);
   }, {});
