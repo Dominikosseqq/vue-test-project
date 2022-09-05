a0aa55a0dc776bceb87141a314ad6cb2bcd94ebb
Dominik Kisek <domminots@gmail.com>
2022-09-05 23:07:51 +0200

    add limiter options + change naming

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,11 @@
-export const endpointsApiGiphy = {
+// all available api endpoints
+export const ENDPOINTS_API_GIPHY = {
   trending: "trending",
   search: "search",
 };
 
-export const keyParamsApiGiphy = ["q", "offset", "limit"];
+// all available params
+export const KEY_PARAMS_API_GIPHY = ["q", "offset", "limit"];
+
+// all available limiters
+export const LIMITER_OPTIONS_API_GIPHY = [10, 20, 30, 50];
