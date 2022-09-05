b6d0599eb2a93f2cb6e1f027d9857eb52eb01d66
Dominik Kisek <domminots@gmail.com>
2022-09-05 23:08:42 +0200

    change logic of apply search query to url

--- src/helpers/params/applyParamsToUrl.ts
+++ src/helpers/params/applyParamsToUrl.ts
@@ -1,35 +1,17 @@
-export const applyParamsToUrl = (history: History, newVal: string): void => {
-  const newQuery = newVal ? `q=${newVal}` : "";
-  const oldQuery = history.state.current;
-  let currentQuery = oldQuery;
-  if (!oldQuery.includes("?")) {
-    currentQuery = `${oldQuery}?${newQuery}`;
-  } else if (!oldQuery.includes("q")) {
-    currentQuery = `${oldQuery}${newQuery.length ? `&${newQuery}` : ""}`;
-  }
-  if (oldQuery.includes("q")) {
-    const newCurrent = oldQuery
-      .replace("/?", "")
-      .split("&")
-      .map((param: string) => {
-        if (param.includes("q=")) {
-          return newQuery;
-        }
-        return param;
-      })
-      .join("&");
+import { LocationQueryRaw } from "vue-router";
 
-    currentQuery = `/?${newCurrent}`;
-  }
-  if (currentQuery.startsWith("/?&")) {
-    currentQuery = currentQuery.replace("/?&", "/?");
-  }
-  if (currentQuery.at(-1) === "?" || currentQuery.at(-1) === "&") {
-    currentQuery = currentQuery.slice(0, -1);
-  }
-  if (currentQuery.length < 3) {
-    currentQuery = "/";
-  }
-  console.log(666, currentQuery);
-  history.pushState(history.state, "", `${currentQuery}`);
+/**
+ * Function that only changes url without redirect from actual query
+ * @param query {LocationQueryRaw}
+ */
+export const applyParamsToUrl = (query: LocationQueryRaw): void => {
+  const link = Object.keys(query)
+    .filter((key: string) => query[key] !== undefined && query[key] !== "")
+    .map((key: string) => {
+      const value = query[key];
+      return `${key}=${value}`;
+    })
+    .join("&");
+
+  history.replaceState({}, "", `/?${link}`);
 };
